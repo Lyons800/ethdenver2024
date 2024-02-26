5112a4a0f097d44356d30db82fc5b34b9de50228
Lyons800 <oisinlyons13@gmail.com>
2024-02-26 16:40:40 -0700

    remove caching stuff

--- next.config.js
+++ next.config.js
@@ -3,10 +3,6 @@ const withPWA = require('next-pwa')({
   disable: process.env.NODE_ENV === 'development',
   // add your own icons to src/app/manifest.ts
   // to re-generate manifest.json, you can visit https://tomitm.github.io/appmanifest/
-  cacheOnFrontEndNav: true,
-  runtimeCaching: true,
-  reloadOnOnline: true,
-  swcMinify: true,
 });
 
 /** @type {import('next').NextConfig} */
