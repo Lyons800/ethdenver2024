165d78f71549500d61eeea3d758fa6fb52a8bf7f
Lyons800 <oisinlyons13@gmail.com>
2024-02-26 20:07:15 -0700

    map

--- src/lib/components/map.tsx
+++ src/lib/components/map.tsx
@@ -6,39 +6,7 @@ import classes from './map.module.css';
 
 const MapComponent = ({ latitude, longitude }) => {
   const mapboxToken = process.env.NEXT_PUBLIC_MAPBOX_API_TOKEN;
-  export default function MapComponent({ latitude, longitude, token }) {
-    const mapRef = useRef(null);
-    const [viewState, setViewState] = useState({
-      latitude: 35.668641, // Default latitude
-      longitude: 139.750567, // Default longitude
-      zoom: 10,
-    });
 
-    useEffect(() => {
-      if (latitude && longitude) {
-        setViewState({
-          ...viewState,
-          latitude,
-          longitude,
-        });
-      }
-    }, [latitude, longitude]);
-
-    return (
-      <Map
-        ref={mapRef}
-        mapboxAccessToken={token}
-        mapStyle="mapbox://styles/mapbox/streets-v12"
-        style={{ width: '100%', height: '100%' }}
-        {...viewState}
-        onMove={(evt) => setViewState(evt.viewState)}
-      >
-        {latitude && longitude && (
-          <Marker latitude={latitude} longitude={longitude} />
-        )}
-      </Map>
-    );
-  }
   return (
     <div className={classes.mainStyle}>
       <Map
