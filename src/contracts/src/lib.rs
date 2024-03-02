37c016bce55664bda980ba7fdfdd1834ee6091ef
Kris O'Shea <minimallifter@protonmail.com>
2024-03-01 20:42:21 -0700

    initial working event nft contract

--- src/contracts/src/lib.rs
+++ src/contracts/src/lib.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::{LazyOption, LookupMap, UnorderedMap, UnorderedSet};
 use near_sdk::json_types::{Base64VecU8, U128};
@@ -5,14 +6,14 @@ use near_sdk::serde::{Deserialize, Serialize};
 use near_sdk::{
     env, near_bindgen, AccountId, Balance, CryptoHash, PanicOnDefault, Promise, PromiseOrValue,
 };
-use std::collections::HashMap;
 
-pub use crate::approval::*;
 use crate::internal::*;
 pub use crate::metadata::*;
 pub use crate::mint::*;
 pub use crate::nft_core::*;
+pub use crate::approval::*;
 pub use crate::royalty::*;
+pub use crate::events::*;
 
 mod internal;
 mod approval; 
@@ -21,6 +22,12 @@ mod metadata;
 mod mint; 
 mod nft_core; 
 mod royalty; 
+mod events;
+
+/// This spec can be treated like a version of the standard.
+pub const NFT_METADATA_SPEC: &str = "1.0.0";
+/// This is the name of the NFT standard we're using
+pub const NFT_STANDARD_NAME: &str = "nep171";
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
@@ -63,8 +70,8 @@ impl Contract {
     */
     #[init]
     pub fn new_default_meta(owner_id: AccountId) -> Self {
-          //calls the other function "new: with some default metadata and the owner_id passed in 
-          Self::new(
+        //calls the other function "new: with some default metadata and the owner_id passed in 
+        Self::new(
             owner_id,
             NFTContractMetadata {
                 spec: "nft-1.0.0".to_string(),
@@ -81,7 +88,7 @@ impl Contract {
     /*
         initialization function (can only be called once).
         this initializes the contract with metadata that was passed in and
-        the owner_id.
+        the owner_id. 
     */
     #[init]
     pub fn new(owner_id: AccountId, metadata: NFTContractMetadata) -> Self {
@@ -105,3 +112,6 @@ impl Contract {
         this
     }
 }
+
+#[cfg(test)]
+mod tests;
