51248406d35780822fadacd3423127f1de567cb2
Earl Potters <earl.potters@gmail.com>
2024-03-02 04:54:14 -0700

    add near helpers

--- src/app/contract-call/page.tsx
+++ src/app/contract-call/page.tsx
@@ -6,31 +6,47 @@ import { initNearContract } from '@/near/near-contract-helper';
 
 import { useWallet } from '@/context/wallet-context'; // Adjust the import path as needed
 
+import { useNearContract } from '@/hooks/use-near-contract'; // Adjust the import path as needed
+
 interface Contract {
   get_name(): Promise<string>;
 }
 
 export default function InitContractButton() {
   const { wallet, setWallet, isSignedIn, setIsSignedIn } = useWallet();
+  const contractId = 'ethprince.testnet'; // Replace with your contract ID
+  const { nftTotalSupply, nftSupplyForOwner, nftToken, nftMint, nftTokens} =
+    useNearContract(contractId, wallet);
 
   const handleClick = async () => {
     try {
       console.log('wallet', wallet);
       // const env = 'testnet';
+
       // debugger;
-      // const contract = await initNearContract('testnet', wallet);
+      // const accountId = wallet.accountId;
+      // const result = await nftSupplyForOwner(accountId);
 
-     const result = await wallet.viewMethod({contractId: 'ethprince.testnet', method: 'nft_total_supply', args: {}})
+      // const result = await nftToken('1')
 
-     console.log('result', result);
+      // const metadata = {
+      //   token_id: '0',
+      //   receiver_id: "'$ID'",
+      //   token_metadata: {
+      //     title: 'Some Art',
+      //     description: 'My NFT media',
+      //     media:
+      //       'https://bafkreiabag3ztnhe5pg7js4bj6sxuvkz3sdf76cjvcuqjoidvnfjz7vwrq.ipfs.dweb.link/',
+      //     copies: 1,
+      //   },
+      // };
 
-    //  const result =  await contract.nft_total_supply()
-    //   console.log('result', result);
+      // const result = await nftMint('3', metadata, wallet.accountId);
 
-      // const name = await contract.get_name()
-      // console.log('contract name', name);
+      const result = await nftTokens('1');
 
-      // Assuming initNearContract() resolves with some contract instance or similar
+      console.log('result', result);
+
       toast({
         title: 'Contract initialized successfully with name:',
         description: `The contract is now ready for interaction. `,
@@ -45,6 +61,10 @@ export default function InitContractButton() {
     }
   };
 
+  // if wallet is not signed in, return null
+  if (!isSignedIn) {
+    return <div>Waallet Not signed in</div>;
+  }
   return (
     <Button onClick={handleClick} variant="outline" size="default">
       Initialize NEAR Contract
--- src/hooks/use-near-contract.tsx
+++ src/hooks/use-near-contract.tsx
@@ -1,98 +1,109 @@
 import { useState, useEffect, useCallback } from 'react';
 import { initNearContract } from '@/near/near-contract-helper';
 
-export default function useNearContract(env: string) {
-  const [contract, setContract] = useState<any>(null);
-  const [loading, setLoading] = useState(true);
+import { useWallet } from '@/context/wallet-context'; // Adjust the import path as needed
 
-  // Initialize contract
-  useEffect(() => {
-    setLoading(true);
-    initNearContract(env)
-      .then((contract) => setContract(contract))
-      .catch(console.error)
-      .finally(() => setLoading(false));
-  }, [env]);
-
-  // Wrap contract calls to handle loading and errors
-  const callContract = useCallback(
-    async (methodName: string | number, args = {}) => {
-      if (!contract) {
-        console.error('Contract is not initialized');
-        return null;
-      }
-      try {
-        if (contract[methodName]) {
-          return await contract[methodName](args);
-        } else {
-          console.error(`Method ${methodName} does not exist on the contract`);
-        }
-      } catch (error) {
-        console.error('Error calling contract method:', error);
-      }
-    },
-    [contract]
-  );
-
+export function useNearContract(contractId: string, wallet: any) {
+  // const contractId = 'ethprince.testnet'; // Replace with your contract ID
   return {
-    loading,
-    contract,
-    // Contract method wrappers
-    getName: () => callContract('get_name'),
-    getMembers: (group: string) => callContract('get_members', { group }),
-    getUnreadMessages: (account: any) =>
-      callContract('unread_messages', { account }),
-    getMessages: ({
-      accounts,
-      group,
-      offset,
-      length,
-    }: {
-      accounts: any;
-      group: any;
-      offset: any;
-      length: any;
-    }) => callContract('get_messages', { accounts, group, offset, length }),
-    getChannelInfo: (group: string) => callContract('channel_info', { group }),
-    getGroups: (account: any) => callContract('get_groups', { account }),
-    getCreatedAt: () => callContract('created_at'),
-    join: () => callContract('join'),
-    ping: () => callContract('ping'),
-    createGroup: (group: string) => callContract('create_group', { group }),
-    joinGroup: (group: string) => callContract('join_group', { group }),
-    leaveGroup: (group: string) => callContract('leave_group', { group }),
-    groupInvite: (group: string, account: string) =>
-      callContract('group_invite', { group, account }),
-    sendMessage: ({
-      account,
-      group,
-      message,
-      timestamp,
-      parentMessage,
-    }: {
-      account: any;
-      group: any;
-      message: any;
-      timestamp: any;
-      parentMessage: any;
-    }) =>
-      callContract('send_message', {
-        account,
-        group,
-        message,
-        timestamp,
-        parentMessage,
+    // loading,
+    wallet,
+    // View methods
+    nftTotalSupply: () =>
+      wallet.viewMethod({
+        contractId,
+        method: 'nft_total_supply',
+        args: {},
       }),
-    readMessage: ({
-      account,
-      group,
-      message_id,
-    }: {
-      account: any;
-      group: any;
-      message_id: any;
-    }) => callContract('read_message', { account, group, message_id }),
-    toggleReaction: (message_id: any, reaction: any) =>
-      callContract('toggle_reaction', { message_id, reaction }),
+    nftTokensDefault: () =>
+      wallet.viewMethod({
+        contractId,
+        method: 'nft_tokens',
+        args: {},
+      }),
+    nftTokens: (fromIndex) =>
+      wallet.viewMethod({
+        contractId,
+        method: 'nft_tokens',
+        args: { from_index: fromIndex },
+      }),
+    nftSupplyForOwner: (accountId) =>
+      wallet.viewMethod({
+        contractId,
+        method: 'nft_supply_for_owner',
+        args: { account_id: accountId },
+      }),
+    nftTokensForOwner: (accountId, fromIndex, limit) =>
+      wallet.viewMethod({
+        contractId,
+        method: 'nft_tokens_for_owner',
+        args: { account_id: accountId, from_index: fromIndex, limit: limit },
+      }),
+    nftMetadata: () =>
+      wallet.viewMethod({
+        contractId,
+        method: 'nft_metadata',
+        args: {},
+      }),
+    nftToken: (tokenId) =>
+      wallet.viewMethod({
+        contractId,
+        method: 'nft_token',
+        args: { token_id: tokenId },
+      }),
+
+    // Call methods
+    nftMint: (tokenId, metadata, receiverId) =>
+      wallet.callMethod({
+        contractId,
+        method: 'nft_mint',
+        args: {
+          token_id: tokenId,
+          metadata: metadata,
+          receiver_id: receiverId,
+        },
+        deposit: '6000000000000000000000',
+      }),
+    nftTransfer: (receiverId, tokenId, approvalId, memo) =>
+      wallet.callMethod({
+        contractId,
+        method: 'nft_transfer',
+        args: {
+          receiver_id: receiverId,
+          token_id: tokenId,
+          approval_id: approvalId,
+          memo: memo,
+        },
+      }),
+    nftApprove: (tokenId, accountId, msg) =>
+      wallet.callMethod({
+        contractId,
+        method: 'nft_approve',
+        args: { token_id: tokenId, account_id: accountId, msg: msg },
+      }),
+    nftRevoke: (tokenId, accountId) =>
+      wallet.callMethod({
+        contractId,
+        method: 'nft_revoke',
+        args: { token_id: tokenId, account_id: accountId },
+      }),
+    nftRevokeAll: (tokenId) =>
+      wallet.callMethod({
+        contractId,
+        method: 'nft_revoke_all',
+        args: { token_id: tokenId },
+      }),
+    nftTransferCall: (receiverId, tokenId, approvalId, memo, msg) =>
+      wallet.callMethod({
+        contractId,
+        method: 'nft_transfer_call',
+        args: {
+          receiver_id: receiverId,
+          token_id: tokenId,
+          approval_id: approvalId,
+          memo: memo,
+          msg: msg,
+        },
+      }),
   };
 }
